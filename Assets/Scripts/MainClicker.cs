c54971339bc6ae51bf754773b2f9b004ee2a9cdd
Sam Wu <sjwu@ualberta.ca>
2021-07-28 21:58:39 -0600

    b

--- Assets/Scripts/MainClicker.cs
+++ Assets/Scripts/MainClicker.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MainClicker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject textBox;
 
-    // Update is called once per frame
-    void Update()
+    public void ClickedMainClicker()
     {
-        
+        textBox.SetActive(true);
     }
 }
