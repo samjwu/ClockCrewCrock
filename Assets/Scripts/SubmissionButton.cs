8b5b8e4361f7f91d7ddf5c9767dbae83e1f11c07
Sam Wu <sjwu@ualberta.ca>
2021-08-02 20:41:21 -0600

    b

--- Assets/Scripts/SubmissionButton.cs
+++ Assets/Scripts/SubmissionButton.cs
@@ -13,7 +13,9 @@ public class SubmissionButton : MonoBehaviour
     public GameObject StatusDisplay;
 
     public GameObject SavedDisplay;
+    public AudioSource SaveSound;
     public GameObject BlammedDisplay;
+    public AudioSource BlamSound;
     public GameObject ChanceDisplay;
 
     public GameObject StrawberryButton;
@@ -32,7 +34,7 @@ public class SubmissionButton : MonoBehaviour
     void Start()
     {
         InfoDisplay.GetComponent<Text>().text = "First, there was nothing.\n\n" +
-            "Then, there was StrawberryClock.\n\n\n" +
+            "Then, there was StrawberryClock.\n\n" +
             "StrawberryClock is the true King of the Portal and God of Newgrounds.\n\n" +
             "That Tom Fulp guy is just a lackey who carries out his will.";
         string secondInfoText = "The first legendary submission by StrawberryClock was named 'A'.\n\n" +
@@ -52,6 +54,7 @@ public class SubmissionButton : MonoBehaviour
             TitleDisplay.SetActive(true);
             StatusDisplay.SetActive(true);
             StatusDisplay.GetComponent<Text>().text = "Your submission just got\nBLAMMED!";
+            BlamSound.Play();
             BlammedDisplay.SetActive(true);
             SubmitButton.SetActive(false);
 
@@ -69,9 +72,11 @@ public class SubmissionButton : MonoBehaviour
         } else {
             if (SubmissionManager.SaveChance >= Random.Range(0.0f, 1.0f)) {
                 StatusDisplay.GetComponent<Text>().text = "Your submission just got\nSAVED!";
+                SaveSound.Play();
                 SubmissionManager.SavedSubmissions++;
             } else {
                 StatusDisplay.GetComponent<Text>().text = "Your submission just got\nBLAMMED!";
+                BlamSound.Play();
                 SubmissionManager.BlammedSubmissions++;
             }
         }
--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Clock Crew Crock
 
+*Try saying THAT 10 times really fast*
+
 An interactive retelling of Newgrounds portal history and the Clock Crew from the perspective of Strawberry Clock.
 
 ### crock
 
 Definition
 
-> *From an absolute, totally, indisputably, legitimate source of nollij proofread by men of culture, learning, and academics*
+> *From an absolutely, totally, indisputably, legitimate source of nollij proofread by men of culture, learning, and academia*
 
 1. nonsence, hogwash, bilge, absolute bull, a waste of time and/or money, a sham. It's bogus - totally.
 
