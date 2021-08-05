f4c90b3b40a0ed98a4e752200ab2ab665a4bee39
Sam Wu <sjwu@ualberta.ca>
2021-08-04 22:25:20 -0600

    b

---
+++ Assets/Scripts/MenuOptions.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOptions : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void BeginGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
